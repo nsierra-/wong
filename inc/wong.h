526024ca3bd106971336a48cb015ef8b12399396
Arthur MAURER <amaurer@e1r12p8.42.fr>
2015-02-27 21:15:13 +0100

    header

--- inc/wong.h
+++ inc/wong.h
@@ -3,14 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   wong.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsierra- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: amaurer <amaurer@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 20:43:20 by nsierra-          #+#    #+#             */
-/*   Updated: 2015/02/27 20:43:36 by nsierra-         ###   ########.fr       */
+/*   Updated: 2015/02/27 21:14:05 by amaurer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef WONG_H
 # define WONG_H
+
+# define				DEFAULT_WIDTH		4
+# define				DEFAULT_HEIGHT		4
+# define				DEFAULT_WIN_VALUE	2048
+
+typedef unsigned int	uint;
+
+typedef struct			s_2048
+{
+	uint				width;
+	uint				height;
+	uint				score;
+	uint				**map;
+	uint				win_value;
+	uint				move_count;
+}						t_2048;
 
 #endif
